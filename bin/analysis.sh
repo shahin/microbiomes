38e6fb15239b841bc3b25f46ebe1e4e8d8176ef0
Shahin Saneinejad <user@example.com>
2013-06-11 06:05:45 +0000

    Count matches in a single pass

--- bin/analysis.sh
+++ bin/analysis.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-blastn -query ../data/rrna/stool/${1}.fsa -db ../data/rrna/stool/${2}.fsa -evalue 1e-10 -outfmt '10 ' -max_target_seqs 1 \
-  > result_${1}_${2}.log
+# get unique match rates for each pair of scaffolds
+# | of each unique match rate, take the scaffold names
+# | count the number of times each pair of names appears
+# | take only the pairs that appeared once (had only one match rate)
+read -d '' get_uniform_matches <<"EOF"
+  sort -u \
+    | cut -d, -f1,2 \
+    | sort | uniq -c \
+    | grep '1 '
+EOF
+
+# get scaffold pairs where the first is a subset of the second
+# | take scaffold names
+# | uniquify
+read -d '' get_perfect_matches <<"EOF"
+  grep '100.00' \
+    | cut -d, -f1,2 \
+    | sort -u
+EOF
 
 # get all seqs that had 100 pct matches
-cat result_${1}_${2}.log \
-  | grep '100.00' \
-  | cut -d, -f1 \
-  | sort | uniq \
+head alignments_${1}_${2}_${3}_${4}.log \
+  | cut -d, -f1,2,3 \
+  | tee >(eval $get_uniform_matches > uniform_matches.log ) \
+  | eval $get_perfect_matches \
   > perfect_matches.log
-
-cat result_${1}_${2}.log \
-  | sort | uniq -c \
-  | cut -d' ' -f5 \
-  | sort | uniq -c \
-  | grep '1 ' \
-  > uniform_matches.log
-
